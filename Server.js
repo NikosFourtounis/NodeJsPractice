3b224c9583386a38ed71d9ed2eedf09b8b3d9f1f
N Fourtounis <nickfortune@windowslive.com>
2018-03-23 14:12:42 +0200

    finally

--- Server.js
+++ Server.js
@@ -5,9 +5,13 @@ var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
 
+mongoose.Promise = Promise
+
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 var dbUrl = 'mongodb://user:user@ds219879.mlab.com:19879/nodejspractice'
 
@@ -16,25 +20,41 @@ var Message = mongoose.model('Message', {
     message: String
 })
 
-app.get('/messages',(req, res)=>{
-    Message.find({},(err,messages) =>{
+app.get('/messages', (req, res) => {
+    Message.find({}, (err, messages) => {
         res.send(messages)
     })
 })
 
-app.post('/messages',(req, res)=>{
-    var message = new Message(req.body)
+app.post('/messages', async (req, res) => {
+    try {
+        var message = new Message(req.body)
 
-    message.save ((err) => {
-        if(err)
-            sendStatus(500)
+        var savedMessage = await message.save()
 
-        io.emit('message',req.body)
+        console.log('saved')
+
+        var censored = await Message.findOne({
+            message: 'badword'
+        })
+
+        if (censored)
+            await Message.remove({
+                _id: censored.id
+            })
+        else
+            io.emit('message', req.body)
+
         res.sendStatus(200)
-    })
+    } catch (error) {
+        res.sendStatus(500)
+        return console.log(error)
+    } finally {
+        
+    }
 })
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
     console.log('A user connected')
 })
 
@@ -42,5 +62,5 @@ mongoose.connect(dbUrl, (err) => {
     console.log('Mongo DB Connection', err)
 })
 var server = http.listen(3000, () => {
-    console.log('Server is listening on port',server.address().port)
+    console.log('Server is listening on port', server.address().port)
 })
