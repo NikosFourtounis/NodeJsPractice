2781172efcf3297b79fb6b8fc36bca5cf2f0ea58
N Fourtounis <nickfortune@windowslive.com>
2018-03-23 15:05:08 +0200

    jasmine testing

--- Server.js
+++ Server.js
@@ -25,6 +25,12 @@ app.get('/messages', (req, res) => {
         res.send(messages)
     })
 })
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages)
+    })
+})
 
 app.post('/messages', async (req, res) => {
     try {
